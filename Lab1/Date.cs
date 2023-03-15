43764b2a7c32601bfe6fe66667c010660bc633ef
Admin <Admin@DESKTOP-K3FB6AF>
2023-03-15 12:08:36 +0200

    Editing code

--- Lab1/Date.cs
+++ Lab1/Date.cs
@@ -8,42 +8,31 @@ namespace Lab1
         private int month;
         private int year;
 
-        public int Day
-        {
-            get
-            {
-                return day;
+        public int Day { 
+            get { return day;}
+            set { 
+                day = value; 
+                if (value < 0 || value > 31) { throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and 31."); }
             }
         }
-        public int Month {
-            get
-            {
-                return month;
-            }
-            set
-            {
-                month = value;
-                if (value < 0 || value > 12)
-                {
-                    Console.WriteLine("The month can't be less than 0 or more than 12");
-                    Environment.Exit(0);
-                }
-            }
 
-            get
-            {
-                return year;
-            }
-            set
-            {
-                year = value;
-                if (value < 0 || value > 2023)
-                {
-                    Console.WriteLine("The year can't be less than 0 or more than 2023");
-                    Environment.Exit(0);
-                }
+        public int Month { 
+            get { return month; }
+            set {
+                month = value;
+                if (value < 0 || value > 12) { throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and 12.");}
             }
         }
+
+        public int Year {
+            get { return year; }
+            set {
+                year = value;
+                if (value < 0 || value > 2023) { throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and 2023.");}
+            }
+        }
+
+        public Date() {}
 
         public Date(int day, int month, int year)
         {
@@ -54,88 +43,29 @@ namespace Lab1
 
         public void ShowDate()
         {
-            if (day < 10 && month < 10)
-                Console.WriteLine("The date is: 0{0}.0{1}.{2}", day, month, year);
-            else if (day > 9 && month < 10)
-            {
-                Console.WriteLine("The date is: {0}.0{1}.{2}", day, month, year);
-            }
-
-        public void DayChange()
-        {
-            
-            int newDay = 0;
-            Console.WriteLine("Please, enter the day you want");
-            try
-            {
-                newDay = Convert.ToInt32(Console.ReadLine());
-                if (newDay < 0 || newDay > 31)
-                {
-                    Console.WriteLine("The value is not correct, please, try again");
-                    Environment.Exit(0);
-                }
-            
-            if (newDay < 10)
-                Console.WriteLine("The new date is: 0{0}.0{1}.{2}", newDay, month, year);
-            else if (newDay > 29 && month == 2)
-                Console.WriteLine("It's February, it doesn't have more than 29 days XD");
-            else
-                Console.WriteLine("{0}.0{1}.{2}", newDay, month, year);
-
+            Console.WriteLine($"The date is: {(day < 10?"0"+day:day)}.{(month<10?"0"+month:month)}.{year}");
         }
 
-        public void MonthChange()
+        public int DayChange(int day)
         {
-            Console.WriteLine("Please, enter the month you want");
-            int newMonth = 0;
-
-            try
-            {
-                newMonth = Convert.ToInt32(Console.ReadLine());
-                if (newMonth < 0 || newMonth > 12)
-                {
-                    Console.WriteLine("Month value cannot be higher than 12");
-                    Environment.Exit(0);
-
-                }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Incorrect input type, please, try again");
-                Environment.Exit(0);
-            }
-
-            if (day < 10 && newMonth < 10 )
-                Console.WriteLine("The new date is: 0{0}.0{1}.{2}", day, newMonth, year);
-            else if (day > 10 && newMonth < 10)
-                Console.WriteLine("The new date is: {0}.0{1}.{2}", day, newMonth, year);
-            else if (day < 10 && newMonth > 9 && newMonth < 13)
-                Console.WriteLine("The new date is: 0{0}.{1}.{2}", day, newMonth, year);
-            else if (day > 10 && newMonth > 9 && newMonth < 13)
-                Console.WriteLine("The new date is: {0}.{1}.{2}", day, newMonth, year);
-            else
-                Console.WriteLine("Something went wrong");
+            this.day = day;
+            ShowDate();
+            return day;
         }
 
-        public void YearChange()
+        public int MonthChange(int month)
         {
+            this.month = month;
+            ShowDate();
+            return month;
+        }
 
-            int newYear = day;
-            Console.WriteLine("Please, enter the year you want");
-            try
-            {
-                newYear = Convert.ToInt32(Console.ReadLine());
-                if (newYear < 0 || newYear > 2023)
-                {
-                    Console.WriteLine("Please, enter correct year. The value cannot be less than 0 or more than 2023");
-                    Environment.Exit(0);
-                }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Incorrect input type, please, try again");
-                Environment.Exit(0);
-            }
+        public int YearChange(int year)
+        {
+            this.year = year;
+            ShowDate();
+            return year;
+        }
 
     }
 }
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,63 +10,79 @@ namespace Lab1
 
             Date date = new Date();
 
-            Console.WriteLine("Enter the day");
-            date.Day = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the month");
-            date.Month = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the year");
-            date.Year = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Enter the day");
+                date.Day = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Enter the month");
+                date.Month = Convert.ToInt32(Console.ReadLine());
 
-            date.ShowDate();
-            
+                Console.WriteLine("Enter the year");
+                date.Year = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch (Exception e) { throw new Exception("Format exception", e); }
+
+            date.ShowDate(); 
 
             Console.WriteLine("Do you want to change the date?");
             Console.WriteLine("1 - YES");
             Console.WriteLine("2 - NO");
             Console.WriteLine("Please, input 1 or 2 number");
             
-            
-            try
-            {
-                userInput = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Incorrect input type, please, try again");
-            }
+            try {userInput = Convert.ToInt32(Console.ReadLine());}
+            catch (Exception e) { throw new Exception("Format exception", e); }
 
             if (userInput == 1)
             {
                 Console.WriteLine("What do you want to change?");
                 Console.WriteLine("Day - 1, Month - 2, Year - 3");
 
-                try
-                {
-                    userInput = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Incorrect input type, please, try again");
-                }
+                try { userInput = Convert.ToInt32(Console.ReadLine());}
+                catch (Exception e) { throw new Exception("Format exception", e); }
 
-                if(userInput == 1)
+                if (userInput == 1)
                 {
-                    date.DayChange();
-                } else if (userInput == 2)
+                    int newDay = 0;
+                    Console.WriteLine("Enter the new day value");
+                    try
+                    {
+                        newDay = Convert.ToInt32(Console.ReadLine());
+                        if(newDay < 0 || newDay > 31) { throw new ArgumentOutOfRangeException(); }
+                    }
+                    catch (Exception e) { throw new Exception("Format exception", e); }
+
+                    date.DayChange(newDay);
+                } 
+                else if (userInput == 2)
                 {
-                    date.MonthChange();
-                } else if (userInput == 3)
+                    int newMonth = 0;
+                    Console.WriteLine("Enter the new month value");
+                    try
+                    {
+                        newMonth = Convert.ToInt32(Console.ReadLine());
+                        if (newMonth < 0 || newMonth > 12) { throw new ArgumentOutOfRangeException(); }
+                    }
+                    catch (Exception e) { throw new Exception("Format exception", e); }
+
+                    date.MonthChange(newMonth);
+                } 
+                else if (userInput == 3)
                 {
-                    date.YearChange();
-                }
-                
-                
-            } else
-            {
-                Console.WriteLine("Thanks for using my program! See you soon <3");
-                Environment.Exit(0);
+                    int newYear = 0;
+                    Console.WriteLine("Enter the new year value");
+                    try
+                    {
+                        newYear = Convert.ToInt32(Console.ReadLine());
+                        if (newYear < 0 || newYear > 2023) { throw new ArgumentOutOfRangeException(); }
+                    }
+                    catch (Exception e) { throw new Exception("Format exception", e); }
+
+                    date.YearChange(newYear);
+                }  
             }
+            else{Console.WriteLine("Thanks for using my program! See you soon <3");}
 
         }
     }
